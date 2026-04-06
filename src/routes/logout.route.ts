e9b8f1f8f0af462b92746552be95c489f5ae717f
Aidan <aidanbh16@gmail.com>
2026-04-06 13:35:36 -0400

    added logout route

--- src/routes/logout.route.ts
+++ src/routes/logout.route.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const router = Router();
 
-router.post("/logout", (req, res) => {
+export default router.post("/logout", (req, res) => {
     res.clearCookie("token");
   
     return res.json({ message: "Logged out successfully" });
