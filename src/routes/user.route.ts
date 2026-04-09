37979db549c94c38ac75422b417a58e302c61cbe
Aidan <aidanbh16@gmail.com>
2026-04-09 12:02:59 -0400

    removed unnecessary logs for testing

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -6,8 +6,6 @@ const router = Router();
 
 router.get("/user", (req, res) => {
     const token = req.cookies.user;
-    console.log("COOKIES:", req.cookies);
-    console.log("TOKEN:", req.cookies.user);
 
     if (!token) {
         return res.status(401).json({ user: null });
