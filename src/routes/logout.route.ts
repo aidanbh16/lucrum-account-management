0693b655f580caafc3e52f85a278ff7d03d5c174
Aidan <aidanbh16@gmail.com>
2026-04-22 15:17:20 -0400

    logout change

--- src/routes/logout.route.ts
+++ src/routes/logout.route.ts
@@ -4,6 +4,13 @@ const router = Router();
 
 router.post("/logout", (req, res) => {
     res.clearCookie("user");
+
+    res.clearCookie("user", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        domain: ".lucrumproject.com",
+    });
   
     return res.json({ message: "Logged out successfully" });
 });
